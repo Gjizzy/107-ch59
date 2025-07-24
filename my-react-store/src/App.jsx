34afc638ee4ae5cf82f987519fe190238a1d8d9f
AdrianAguinaga <adrian.aguinaga@uabc.edu.mx>
2025-07-23 19:59:09 -0700

    first commit

---
+++ my-react-store/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Navbar'
+
+function App() {
+  
+  return (
+    <>
+     <Header></Header>
+     <Footer/>
+    
+    </>
+  )
+}
+
+export default App
