a21de76cd87c52bad9d9fcffce8b2d11d1cc6204
Gjizzy <Yaquelin.Lopez@sdgku.edu>
2025-08-14 17:23:33 -0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import Footer from './components/Footer
+import Header from './components/Navbar'
+import QuantityPicker from './components/QuantityPicker'
+
+function App() {
+
+
+    return (
+        <>
+        <Header></Header>
+        <QuantityPicker/>
+        <Footer/>
+
+        </>
+    )
+}
+
+export default App
+
+    
